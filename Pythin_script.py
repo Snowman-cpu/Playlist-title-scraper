de381f3e3fb3a9c16cb47875da087be118d631ce
Snowman-cpu <Waghmareuday29@gmail.com>
2025-08-04 15:44:30 +0530

    Make playlist URL a user input instead of hard-coded
    Replaced the hard-coded playlist URL with a user input prompt.
    
    This makes the script interactive and allows users to specify a URL without modifying the source code, improving usability.

--- Pythin_script.py
+++ Pythin_script.py
@@ -1,6 +1,7 @@
 import yt_dlp
 
-playlist_url = "https://www.youtube.com/playlist?list=PLU6SqdYcYsfJPF-4HphQQ8OceDtqhlSW8" # put your url here this is a sample one 
+a = ("Enter your playlist url: \n")
+playlist_url = a # You don't need to manually change the url in the now 
 
 ydl_opts = {
     'quiet': True,
@@ -16,3 +17,22 @@ with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         print(f"{i}.{entry.get('title')}")
         
          
+         
+import yt_dlp
+
+a = input("Enter your url: \n")
+playlist_url = a
+
+ydl_ops = {
+    'quiet' : True,
+    'extract_flat': True,
+    'skip_download':True,
+}
+
+with yt_dlp.YoutubeDL(ydl_ops) as ydl:
+    info = ydl.extract_info(playlist_url,download = False)
+    print(f"Playlist Titles: {info.get('title')}\n")
+    print(f"Video Titles \n")
+    
+    for i,entry in enumerate(info.get('entries',[]),start=1):
+        print(f"{i}.{entry.get('title')}")
